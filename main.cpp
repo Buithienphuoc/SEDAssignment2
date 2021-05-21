35eb035207e24df8546bfc5d494773717340f8ea
Buithienphuoc <tphuoc1998@gmail.com>
2021-05-21 20:08:24 +0700

    add my information

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main() {
     Controller controller;
     controller.runProgram();
+    cout << endl
+    << "ASSIGNMENT 2 GROUP OneMember" << endl
+    << "s3634831@rmit.edu.vn, Phuoc, Bui";
     return 0;
 }
